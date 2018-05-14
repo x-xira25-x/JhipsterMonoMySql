63871dd97bafa088c2786f2069eea5fcf806a87e
Hofmann Jenny <jenny.hofmann@he-arc.ch>
2018-05-14 10:13:12 +0200

    Suppression entité blog

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,11 +1,11 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { JhipsterTestMonoBlogModule } from './blog/blog.module';
+
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
-        JhipsterTestMonoBlogModule,
+
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
