0e67a5fae385a0bc41c63964ae97be712f78754c
x-xira25-x <32196605+x-xira25-x@users.noreply.github.com>
2018-05-17 13:58:50 +0200

    ajout du client pour les actions dans registrer

--- src/main/webapp/app/account/register/register.component.ts
+++ src/main/webapp/app/account/register/register.component.ts
@@ -3,7 +3,8 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 import { Register } from './register.service';
-import { LoginModalService, EMAIL_ALREADY_USED_TYPE, LOGIN_ALREADY_USED_TYPE } from '../../shared';
+import {LoginModalService, EMAIL_ALREADY_USED_TYPE, LOGIN_ALREADY_USED_TYPE, UserService} from '../../shared';
+import {Client, ClientService} from "../../entities/client";
 
 @Component({
     selector: 'jhi-register',
@@ -19,18 +20,22 @@ export class RegisterComponent implements OnInit, AfterViewInit {
     registerAccount: any;
     success: boolean;
     modalRef: NgbModalRef;
+    client: Client;
 
     constructor(
         private loginModalService: LoginModalService,
         private registerService: Register,
         private elementRef: ElementRef,
-        private renderer: Renderer
+        private renderer: Renderer,
+        private clientService : ClientService,
+        private userService : UserService
     ) {
     }
 
     ngOnInit() {
         this.success = false;
         this.registerAccount = {};
+        this.client = {};
     }
 
     ngAfterViewInit() {
@@ -48,6 +53,21 @@ export class RegisterComponent implements OnInit, AfterViewInit {
             this.registerAccount.langKey = 'en';
             this.registerService.save(this.registerAccount).subscribe(() => {
                 this.success = true;
+                //retourne le user
+                this.userService.find(this.registerAccount.login).subscribe(resp=>{
+                    console.log('essai '+ resp.body.login);
+                    this.client.user =resp.body;
+                    console.info('login client :' +this.client.user.login);
+                    // appeler la sauvegarde du client
+
+                    console.info('appel create client');
+                    this.clientService.create(this.client).subscribe(resp=>{
+                        console.log(resp.body.id);
+
+                    });
+                });
+
+
             }, (response) => this.processError(response));
         }
     }
