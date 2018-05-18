529c99781319d6ec5a92cb3be1bbd207298dba37
x-xira25-x <32196605+x-xira25-x@users.noreply.github.com>
2018-05-18 15:49:29 +0200

    Revert "Revert "ajout des fonctions pour avoir liste des type client""
    This reverts commit 540b9af080967aa561c62269003d70f907ed2473.

--- src/main/webapp/app/account/register/register.component.ts
+++ src/main/webapp/app/account/register/register.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, AfterViewInit, Renderer, ElementRef } from '@angular/core';
-import { HttpErrorResponse } from '@angular/common/http';
+import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 import { Register } from './register.service';
 import {LoginModalService, EMAIL_ALREADY_USED_TYPE, LOGIN_ALREADY_USED_TYPE, UserService} from '../../shared';
 import {Client, ClientService} from "../../entities/client";
+import {TypeClient, TypeClientService} from "../../entities/type-client";
+import {JhiAlertService} from "ng-jhipster";
 
 @Component({
     selector: 'jhi-register',
@@ -21,6 +23,7 @@ export class RegisterComponent implements OnInit, AfterViewInit {
     success: boolean;
     modalRef: NgbModalRef;
     client: Client;
+    typeclients: TypeClient[];
 
     constructor(
         private loginModalService: LoginModalService,
@@ -28,7 +31,9 @@ export class RegisterComponent implements OnInit, AfterViewInit {
         private elementRef: ElementRef,
         private renderer: Renderer,
         private clientService : ClientService,
-        private userService : UserService
+        private userService : UserService,
+        private typeClientService: TypeClientService,
+        private jhiAlertService: JhiAlertService,
     ) {
     }
 
@@ -36,6 +41,8 @@ export class RegisterComponent implements OnInit, AfterViewInit {
         this.success = false;
         this.registerAccount = {};
         this.client = {};
+        this.typeClientService.query()
+            .subscribe((res: HttpResponse<TypeClient[]>) => { this.typeclients = res.body; }, (res: HttpErrorResponse) => this.onError(res.message));
     }
 
     ngAfterViewInit() {
@@ -76,6 +83,25 @@ export class RegisterComponent implements OnInit, AfterViewInit {
         this.modalRef = this.loginModalService.open();
     }
 
+    trackTypeClientByNom(index: number, item: TypeClient) {
+        console.log("nom Type client")
+        return item.nom;
+    }
+
+    getSelected(selectedVals: Array<any>, option: any) {
+        if (selectedVals) {
+            for (let i = 0; i < selectedVals.length; i++) {
+                if (option.id === selectedVals[i].id) {
+                    return selectedVals[i];
+                }
+            }
+        }
+        return option;
+    }
+
+    private onError(error: any) {
+        this.jhiAlertService.error(error.message, null, null);
+    }
     private processError(response: HttpErrorResponse) {
         this.success = null;
         if (response.status === 400 && response.error.type === LOGIN_ALREADY_USED_TYPE) {
