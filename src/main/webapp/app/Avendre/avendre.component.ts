f6f7c1347c4d0050531af7c2c4a727c0d1e5ca94
x-xira25-x <32196605+x-xira25-x@users.noreply.github.com>
2018-05-23 09:49:14 +0200

    ajout méthode pour liste biens a vendre

--- src/main/webapp/app/Avendre/avendre.component.ts
+++ src/main/webapp/app/Avendre/avendre.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Bien} from "../entities/bien/bien.model";
+import {HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {JhiAlertService, JhiDataUtils, JhiEventManager} from "ng-jhipster";
+import {BienService} from "../entities/bien/bien.service";
+import {Principal} from "../shared";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'jhi-avendre',
@@ -6,11 +12,42 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class AvendreComponent implements OnInit {
+    biens: Bien[];
+    currentAccount: any;
+    eventSubscriber: Subscription;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+      private bienService: BienService,
+      private jhiAlertService: JhiAlertService,
+      private dataUtils: JhiDataUtils,
+      private eventManager: JhiEventManager,
+      private principal: Principal
+  ) {
 
   }
+
+    loadAll() {
+        this.bienService.query().subscribe(
+            (res: HttpResponse<Bien[]>) => {
+                this.biens = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
+    ngOnInit() {
+        this.loadAll();
+        console.log("load")
+        this.principal.identity().then((account) => {
+            this.currentAccount = account;
+        });
+        this.registerChangeInBiens();
+    }
+    registerChangeInBiens() {
+        this.eventSubscriber = this.eventManager.subscribe('bienListModification', (response) => this.loadAll());
+    }
+
+    private onError(error) {
+        this.jhiAlertService.error(error.message, null, null);
+    }
 
 }
