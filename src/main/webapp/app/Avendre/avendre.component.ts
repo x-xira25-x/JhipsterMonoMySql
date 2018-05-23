0b57fc034d2c49a1a11a8cb683a642036fbdc669
x-xira25-x <32196605+x-xira25-x@users.noreply.github.com>
2018-05-23 09:36:33 +0200

    ajout component avendre

---
+++ src/main/webapp/app/Avendre/avendre.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-avendre',
+  templateUrl: './avendre.component.html',
+  styles: []
+})
+export class AvendreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
