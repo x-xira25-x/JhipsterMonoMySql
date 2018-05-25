e9d5abbe6c336dab594f0f6ae531d16e39444862
x-xira25-x <32196605+x-xira25-x@users.noreply.github.com>
2018-05-25 15:31:13 +0200

    ajout carousel avec 3 images

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
-import {map} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
+
 
 import { Account, LoginModalService, Principal } from '../shared';
 
@@ -17,30 +16,23 @@ import { Account, LoginModalService, Principal } from '../shared';
 export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
-    images: Array<string>;
+
 
     constructor(
         private principal: Principal,
         private loginModalService: LoginModalService,
         private eventManager: JhiEventManager,
-        private _http: HttpClient
+
     ) {
     }
-    private _randomImageUrls(images: Array<{id: number}>): Array<string> {
-        return [1, 2, 3].map(() => {
-            const randomId = images[Math.floor(Math.random() * images.length)].id;
-            return `https://picsum.photos/900/500?image=${randomId}`;
-        });
-    }
+
 
     ngOnInit() {
         this.principal.identity().then((account) => {
             this.account = account;
         });
         this.registerAuthenticationSuccess();
-        this._http.get('https://picsum.photos/list')
-            .pipe(map((images: Array<{id: number}>) => this._randomImageUrls(images)))
-            .subscribe(images => this.images = images);
+
     }
 
 
