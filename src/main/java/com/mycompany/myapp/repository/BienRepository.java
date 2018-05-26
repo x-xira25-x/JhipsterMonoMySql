ea25c8c1080afea956065a09db4080daef2d5971
Hofmann Jenny <jenny.hofmann@he-arc.ch>
2018-05-26 16:07:39 +0200

    ajout fonctions bien visite
    getAllbiensAvendre (bien)
    GetallBiensAvendre (visite)

--- src/main/java/com/mycompany/myapp/repository/BienRepository.java
+++ src/main/java/com/mycompany/myapp/repository/BienRepository.java
@@ -1,9 +1,13 @@
 package com.mycompany.myapp.repository;
 
 import com.mycompany.myapp.domain.Bien;
+import com.mycompany.myapp.domain.Visite;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
+
+import java.util.List;
 
 
 /**
@@ -12,5 +16,10 @@ import org.springframework.data.jpa.repository.*;
 @SuppressWarnings("unused")
 @Repository
 public interface BienRepository extends JpaRepository<Bien, Long> {
+
+    @Query("Select bien from Bien bien where bien.vendu='n'")
+    List<Bien> findAllAvendre();
+    @Query ("select visite from Bien bien, Visite visite where bien.id =:idBien")
+    List<Visite>findAllBiensVisites(@Param("idBien") Long idBien);
 
 }
