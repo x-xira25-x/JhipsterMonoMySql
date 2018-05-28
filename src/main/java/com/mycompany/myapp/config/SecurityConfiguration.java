9f1224eaa30bea044cd28961624d05ce417ca943
x-xira25-x <32196605+x-xira25-x@users.noreply.github.com>
2018-05-28 15:30:24 +0200

    permit all pour api bien

--- src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
@@ -100,7 +100,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/profile-info").permitAll()
             .antMatchers("/api/type-clients").permitAll()
-            .antMatchers("/api/biens").permitAll()
+            .antMatchers("/api/biens/**").permitAll()
             .antMatchers("/api/**").authenticated()
 
             .antMatchers("/management/health").permitAll()
