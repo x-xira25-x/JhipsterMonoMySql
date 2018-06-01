8da5a961e96793e77db1f7b5733ed1a4ab6e5f7c
x-xira25-x <32196605+x-xira25-x@users.noreply.github.com>
2018-06-01 08:48:22 +0200

    ajout de l'onglet + page présentation

---
+++ src/main/webapp/app/presentation/presentation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-presentation',
+  templateUrl: './presentation.component.html',
+  styles: []
+})
+export class PresentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/main/webapp/app/presentation/presentation.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {avendrePopupRoute, avendreRoute} from "../Avendre/avendre.route";
+import {presentationRoute} from "./presentation.route";
+import {JhipsterTestMonoSharedModule} from "../shared";
+import {RouterModule} from "@angular/router";
+import {PresentationComponent} from "./presentation.component";
+
+
+const ENTITY_STATES = [
+    ...presentationRoute
+
+];
+@NgModule({
+  imports: [
+    CommonModule,
+      JhipsterTestMonoSharedModule,
+      RouterModule.forChild(ENTITY_STATES)
+  ],
+  declarations: [
+      PresentationComponent
+  ],
+    entryComponents: [
+        PresentationComponent
+        ]
+})
+
+
+export class PresentationModule { }
