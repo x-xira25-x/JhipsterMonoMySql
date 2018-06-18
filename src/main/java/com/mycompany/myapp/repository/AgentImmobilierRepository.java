578258da6c86c4101ac08dcc78f319508dc8fcc3
Hofmann Jenny <jenny.hofmann@he-arc.ch>
2018-06-18 13:00:26 +0200

    ajout requete pour trouver l'agent avec l'id du user

--- src/main/java/com/mycompany/myapp/repository/AgentImmobilierRepository.java
+++ src/main/java/com/mycompany/myapp/repository/AgentImmobilierRepository.java
@@ -1,6 +1,7 @@
 package com.mycompany.myapp.repository;
 
 import com.mycompany.myapp.domain.AgentImmobilier;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
@@ -15,5 +16,8 @@ public interface AgentImmobilierRepository extends JpaRepository<AgentImmobilier
 
     @Query("select agent_immobilier from AgentImmobilier agent_immobilier where agent_immobilier.user.login = ?#{principal.username}")
     List<AgentImmobilier> findByUserIsCurrentUser();
+
+    @Query ("select agentImmobilier from AgentImmobilier agentImmobilier  where agentImmobilier.user.id=:idUser")
+    AgentImmobilier findIdAgentImmobilier(@Param("idUser")Long idUser);
 
 }
